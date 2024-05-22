538c2ccab3417cc2026d5bc9b4217626995daa7d
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2024-05-21 21:06:09 -0300

    primeiro commit

---
+++ aula03/aula01.js
@@ -0,0 +1,18 @@
+var qg = prompt("DIGITE A QTD DE GOLS")
+var qc = prompt("DIGITE A QTD DE PASSOS CERTOS")
+var qe = prompt("DIGITE A QTD DE PASSOS ERRADOS")
+
+var qt = (qg * 50) + (qc * 10) + (qe * -5)
+
+           if (qt <50){
+            document.write("PÉSSIMA PARTIDA");
+           }else if(qt >= 50&& qt <100){
+            document.write("partida ruim");
+           }else if(qt >=100 && qt<150){
+            DOMExceptionocument.write("FEZ O BASICO");
+           }else if (qt >=150 && qt<200){
+            document.write("FOI BEM NA PARTIDA");
+           }else{ 
+            document.write("JOGOU DEMAIS");
+           }
+                                                                                                                         
---
+++ aula03/aula02.js
@@ -0,0 +1,10 @@
+var nome = "Fellipe Sant Anna Cota "
+var idade = 32
+var eProfessor = true
+var dataNascimento 
+
+
+document.write(typeof nome, "<br/>")
+document.write(typeof dataNascimento, "<br/>")
+document.write(typeof eProfessor,"<br/>")
+document.write(typeof idade,"<br/>" )
